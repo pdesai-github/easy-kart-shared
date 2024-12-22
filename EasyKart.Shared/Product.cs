fc05e02da37397de79ec2fd01b47303d05336134
Pradipkumar Desai <pdesai.programming@gmail.com>
2024-12-22 10:42:10 +0530

    Add project files.

---
+++ EasyKart.Shared/Product.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace EasyKart.Shared
+{
+    public class Product
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("price")]
+        public decimal Price { get; set; }
+
+        [JsonProperty("category")]
+        public string Category { get; set; }
+
+        [JsonProperty("image")]
+        public string Image { get; set; }
+    }
+}
