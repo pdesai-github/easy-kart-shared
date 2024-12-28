5dda8a111b68622a0380e665bfb487a8b1fe2926
Pradipkumar Desai <pdesai.programming@gmail.com>
2024-12-28 11:55:40 +0530

    [JsonIgnore]

--- EasyKart.Shared/Models/OrderItem.cs
+++ EasyKart.Shared/Models/OrderItem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace EasyKart.Shared.Models
@@ -17,6 +18,8 @@ namespace EasyKart.Shared.Models
         public Guid ProductId { get; set; } // Dictionary key
         [Required]
         public int Quantity { get; set; } // Dictionary value
+        
+        [JsonIgnore]
         public Order Order { get; set; }
     }
 }
