4c17d705ab015c8ba25ffc96e4730e518a502aea
Pradipkumar Desai <pdesai.programming@gmail.com>
2024-12-28 10:50:46 +0530

    Order model changes

--- EasyKart.Shared/Models/Order.cs
+++ EasyKart.Shared/Models/Order.cs
@@ -7,18 +7,49 @@ namespace EasyKart.Shared.Models
     public class Order
     {
         [Key]
-        public Guid Id { get; set; }
+        public Guid Id { get; private set; }
 
         [Required]
-        public List<CartItem> Items { get; set; }
+        public List<OrderItem> Items { get; private set; }
 
         [Required]
-        public Guid UserId { get; set; }
+        public Guid UserId { get; private set; }
 
         [Required]
-        public decimal Price { get; set; }       
-        
-        public string Status { get; set; }
+        public decimal Price { get; private set; }
+
+        [Required]
+        public DateTime CreatedDate { get; private set; }
+
+        public string Status { get; private set; }
+
+        public Order(Guid userId, Cart cart)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            Items = new List<OrderItem>();
+            Price = cart.Price;
+            CreatedDate = DateTime.Now;
+            Status = "Pending";
+            AddItems(cart);
+        }
+
+        public void AddItems(Cart cart)
+        {
+           cart.Items.ForEach(item =>
+           {
+               Items.Add(new OrderItem
+               {
+                   ProductId = item.Product.Id,
+                   Quantity = item.Quantity
+               });
+           });
+        }
+
+        public void UpdateStatus(string status)
+        {
+            Status = status;
+        }
 
     }
 }
---
+++ EasyKart.Shared/Models/OrderItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyKart.Shared.Models
+{
+    public class OrderItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public Guid OrderId { get; set; } // Foreign key to Order table
+        [Required]
+        public Guid ProductId { get; set; } // Dictionary key
+        [Required]
+        public int Quantity { get; set; } // Dictionary value
+        public Order Order { get; set; }
+    }
+}
