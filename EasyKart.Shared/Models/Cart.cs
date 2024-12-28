253a607294fe38a4a57a60e4bd37c099149ae7ec
Pradipkumar Desai <pdesai.programming@gmail.com>
2024-12-28 08:53:22 +0530

    Cart encapsulation

--- EasyKart.Shared/Models/Cart.cs
+++ EasyKart.Shared/Models/Cart.cs
@@ -6,15 +6,56 @@ namespace EasyKart.Shared.Models
     public class Cart
     {
         [JsonProperty("id")]
-        public Guid Id { get; set; }
+        public Guid Id { get; private set; }
 
         [JsonProperty("items")]
-        public List<CartItem> Items { get; set; }
+        public List<CartItem> Items { get; private set; }
 
         [JsonProperty("userId")]
-        public Guid UserId { get; set; }
+        public Guid UserId { get; private set; }
 
         [JsonProperty("price")]
-        public decimal Price { get; set; }
+        public decimal Price { get; private set; }
+
+        public Cart(Guid userId)
+        {
+            Id = Guid.NewGuid();
+            UserId = userId;
+            Items = new List<CartItem>();
+            Price = 0;
+        }
+
+        public void calculatePrice()
+        {
+            Price = Items.Sum(item => item.Product.Price * item.Quantity);
+        }
+
+        public void addProduct(Product product, int quantity)
+        {
+            var item = Items.FirstOrDefault(i => i.Product.Id == product.Id);
+            if (item == null)
+            {
+                Items.Add(new CartItem
+                {
+                    Product = product,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                item.Quantity += quantity;
+            }
+            calculatePrice();
+        }
+
+        public void removeProduct(Product product)
+        {
+            var item = Items.FirstOrDefault(i => i.Product.Id == product.Id);
+            if (item != null)
+            {
+                Items.Remove(item);
+                calculatePrice();
+            }
+        }
     }
 }
