b74d470b0a50fcd3b16d520cde999376a74d05da
Pradipkumar Desai <pdesai.programming@gmail.com>
2025-01-04 08:13:13 +0530

    Event classes added

---
+++ EasyKart.Shared/Events/OrderCreated.cs
@@ -0,0 +1,15 @@
+﻿using EasyKart.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyKart.Shared.Events
+{
+    public class OrderCreated
+    {
+        public DateTime CreatedAt { get; set; }
+        public Order Order { get; set; }
+    }
+}
