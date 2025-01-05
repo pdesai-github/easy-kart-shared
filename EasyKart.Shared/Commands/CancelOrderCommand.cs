5b2109b8422a525ebebfe37a81ee89c66421e601
Pradipkumar Desai <pdesai.programming@gmail.com>
2025-01-05 10:34:25 +0530

    Update order command

---
+++ EasyKart.Shared/Commands/CancelOrderCommand.cs
@@ -0,0 +1,16 @@
+﻿using EasyKart.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyKart.Shared.Commands
+{
+    public class CancelOrderCommand
+    {
+        public DateTime CreatedAt { get; set; }
+        public Order Order { get; set; }
+        public string Reason { get; set; }
+    }
+}
